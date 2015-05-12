dd95b30f104401c49958e92efe97e640ba7133fb
Alexandre Archambault <alexandre.archambault@gmail.com>
2015-05-13 01:13:11 +0200

    Proper path on Windows

--- cli/src/main/scala/jupyter/scala/JupyterScala.scala
+++ cli/src/main/scala/jupyter/scala/JupyterScala.scala
@@ -14,12 +14,21 @@ case class JupyterScala(
   // FIXME Shouldn't sbt-pack put this in system property "prog.name"?
   val progName = "jupyter-scala"
 
+  lazy val isWindows: Boolean =
+    Option(System.getProperty("os.name")).exists(_ startsWith "Windows")
+
+  lazy val progSuffix =
+    if (isWindows) ".bat"
+    else ""
+
   def progPath =
-    Option(System getProperty "prog.home").filterNot(_.isEmpty).map(_ + s"/bin/$progName") getOrElse {
-      Console.err println "Cannot get program home dir, it is likely we are not run through pre-packaged binaries."
-      Console.err println "Please edit the generated file below, and ensure the first item of the 'argv' list points to the path of this program."
-      progName
-    }
+    Option(System getProperty "prog.home").filterNot(_.isEmpty)
+      .map(List(_, "bin", progName + progSuffix).mkString(java.io.File.separator))
+      .getOrElse {
+        Console.err println "Cannot get program home dir, it is likely we are not run through pre-packaged binaries."
+        Console.err println "Please edit the generated file below, and ensure the first item of the 'argv' list points to the path of this program."
+        progName
+      }
 
   ServerApp(ScalaModule.kernelId, ScalaModule.kernel, ScalaModule.kernelInfo, progPath, options)
 }
