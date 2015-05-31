9b215a82920e703cb642a685071f1f1eea5aa332
Alexandre Archambault <alexandre.archambault@gmail.com>
2015-06-01 01:56:21 +0200

    Per scala version branches

--- project/JupyterScalaBuild.scala
+++ project/JupyterScalaBuild.scala
@@ -52,7 +52,7 @@ object JupyterScalaBuild extends Build {
     ),
     scalacOptions += "-target:jvm-1.7",
     crossVersion := CrossVersion.full,
-    crossScalaVersions := Seq("2.10.3", "2.10.4", "2.10.5", "2.11.0", "2.11.1", "2.11.2", "2.11.4", "2.11.5", "2.11.6"),
+    crossScalaVersions := Seq("2.11.0", "2.11.1", "2.11.2", "2.11.4", "2.11.5", "2.11.6"),
     ivyScala := ivyScala.value map { _.copy(overrideScalaVersion = true) },
     fork in test := true,
     fork in (Test, test) := true,
