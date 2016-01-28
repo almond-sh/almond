54e9da1d12f9ad7e628733d7d15f6d097b8a7173
Alexandre Archambault <alexandre.archambault@gmail.com>
2016-01-28 20:44:27 +0100

    Add launchers

--- project/generate-launcher.sh
+++ project/generate-launcher.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-VERSION=0.3.0-SNAPSHOT
+VERSION=0.3.0-M1
 
-  #--no-default \
+if [ "$1" = "--2.10" ]; then
+  SCALA_VERSION=2.10.6
+  OUTPUT=jupyter-scala-2.10
+  EXTRA=
+else
+  SCALA_VERSION=2.11.7
+  OUTPUT=jupyter-scala
+  EXTRA="
+    -I jupyter-scala-compile:org.scala-lang:scala-compiler:$SCALA_VERSION \
+    -I jupyter-scala-compile:org.scalamacros:quasiquotes_2.10:2.0.1 \
+  "
+fi
+
 "$(dirname "$0")/../coursier" bootstrap \
-  com.github.alexarchambault.jupyter:jupyter-scala-cli_2.11.7:$VERSION \
+  com.github.alexarchambault.jupyter:scala-cli_$SCALA_VERSION:$VERSION \
+  -I jupyter-scala-compile:com.github.alexarchambault.jupyter:scala-api_$SCALA_VERSION:$VERSION \
+  -I jupyter-scala-macro:org.scala-lang:scala-compiler:$SCALA_VERSION \
+  $EXTRA \
+  -i jupyter-scala-compile,jupyter-scala-macro \
+  --no-default \
   -r central \
+  -r sonatype:releases \
   -r https://dl.bintray.com/scalaz/releases \
-  -D "\${user.home}/.jupyter-scala/bootstrap/$VERSION" \
-  -f -o jupyter-scala \
+  -D "\${user.home}/.jupyter-scala/bootstrap" \
+  -f -o "$OUTPUT" \
   -M jupyter.scala.JupyterScala
