a5293ccdd1097f587d6402e529c798a351e5e474
Alexandre Archambault <alexandre.archambault@gmail.com>
2017-03-07 17:53:56 +0100

    Tweak JupyterSparkSession

--- spark/core/src/main/scala/jupyter/spark/session/JupyterSparkSession.scala
+++ spark/core/src/main/scala/jupyter/spark/session/JupyterSparkSession.scala
@@ -12,12 +12,16 @@ object JupyterSparkSession {
 
   class Builder(implicit interpApi: InterpAPI, runtimeApi: RuntimeAPI) extends SparkSession.Builder {
 
+    // Most of the stuff that are done in sparkInit, sparkEmr, sparkYarn below change global variables.
+    // Ideally, these things should be kept in Builder, rather than globally. But the global way
+    // also works with Spark 1.x. Alternatively, we could define a basic ad hoc SparkSession for 1.x.
+
     if (!initialized)
       sparkInit()
 
     private var appliedJupyterConf = false
 
-    def jupyterConf(force: Boolean = false): this.type = {
+    def jupyter(force: Boolean = false): this.type = {
       if (force || !appliedJupyterConf) {
         val conf = JupyterSparkContext.withHooks(new SparkConf)
         config(conf)
@@ -27,7 +31,12 @@ object JupyterSparkSession {
     }
 
     def yarn(conf: String = Spark.defaultYarnConf()): this.type = {
+
+      // setting this here, as the hook that set it too in sparkYarn is run too late for SparkSession
+      config("master", "yarn-client")
+
       sparkYarn(conf)
+
       this
     }
 
@@ -38,7 +47,7 @@ object JupyterSparkSession {
 
     override def getOrCreate(): SparkSession = {
 
-      jupyterConf()
+      jupyter()
 
       val session = super.getOrCreate()
 
