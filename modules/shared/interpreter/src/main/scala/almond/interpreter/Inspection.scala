ef7722052130d89a1cd5444b91255ed8c753b186
Alexandre Archambault <alexandre.archambault@gmail.com>
2018-10-04 11:12:40 +0200

    Add helper method

--- modules/shared/interpreter/src/main/scala/almond/interpreter/Inspection.scala
+++ modules/shared/interpreter/src/main/scala/almond/interpreter/Inspection.scala
@@ -1,5 +1,12 @@
 package almond.interpreter
 
+import almond.interpreter.api.DisplayData
+import almond.interpreter.util.DisplayDataOps.toDisplayDataOps
 import argonaut.Json
 
 final case class Inspection(data: Map[String, Json], metadata: Map[String, Json] = Map.empty)
+
+object Inspection {
+  def fromDisplayData(data: DisplayData): Inspection =
+    Inspection(data.jsonData, data.jsonMetadata)
+}
