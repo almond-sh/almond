b1c43198210761b3cebc24c15633a17f1bb4a1f3
Alexandre Archambault <alexandre.archambault@gmail.com>
2018-12-18 18:02:08 +0100

    Validate example notebooks from CI

--- scripts/coursier.sh
+++ scripts/coursier.sh
@@ -5,7 +5,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.."; pwd)"
 mkdir -p "$DIR/target"
 
 if [ ! -e "$DIR/target/coursier" ]; then
-  curl -Lo "$DIR/target/coursier" "https://github.com/coursier/coursier/raw/v1.1.0-M7/coursier"
+  curl -Lo "$DIR/target/coursier" "https://github.com/coursier/coursier/raw/v1.1.0-M9/coursier"
   chmod +x "$DIR/target/coursier"
 fi
 
