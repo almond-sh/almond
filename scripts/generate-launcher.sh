e8999552db6266fb061e3ccdb801737d90e22115
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-01-15 14:26:08 +0100

    Tweak upload-launcher script
    Import back changes made in the same script in coursier (should be
    shared by both repo at some point)

--- scripts/generate-launcher.sh
+++ scripts/generate-launcher.sh
@@ -2,13 +2,13 @@
 set -eu
 
 ALMOND_VERSION="$(git describe --tags --abbrev=0 --always | sed 's@^v@@')"
-SCALA_VERSION="2.12.7" # FIXME Get from .travis.yml or via sbt
+SCALA_VERSION="2.12.8" # FIXME Get from .travis.yml or via sbt
 
 OUTPUT="${OUTPUT:-almond}"
 
 coursier bootstrap \
   -i user -I user:sh.almond:scala-kernel-api_$SCALA_VERSION:$ALMOND_VERSION \
-  --no-default -r central \
+  --no-default -r central -r jitpack \
   sh.almond:scala-kernel_$SCALA_VERSION:$ALMOND_VERSION \
   -o "$OUTPUT" \
   --embed-files=false --sources --default=true \
--- scripts/upload-launcher.sh
+++ scripts/upload-launcher.sh
@@ -1,23 +1,37 @@
 #!/usr/bin/env bash
 set -eu
 
-cd "$(dirname "${BASH_SOURCE[0]}")"
+if [[ ${TRAVIS_TAG} != v* ]]; then
+  echo "Not on a git tag"
+  exit 1
+fi
 
-ALMOND_VERSION="$(git describe --tags --abbrev=0 --always | sed 's@^v@@')"
+export VERSION="$(echo "$TRAVIS_TAG" | sed 's@^v@@')"
 
-RELEASE_ID="$(http "https://api.github.com/repos/almond-sh/almond/releases?access_token=$GH_TOKEN" | jq -r '.[] | select(.name == "v'"$ALMOND_VERSION"'") | .id')"
+# config
+REPO="almond-sh/almond"
+NAME="almond"
+CMD="./scripts/generate-launcher.sh -f --bat=true" # will work once sync-ed to Maven Central
+
+# initial check with Sonatype releases
+mkdir -p target/launcher
+export OUTPUT="target/launcher/$NAME"
+$CMD -r sonatype:releases
+
+
+# actual script
+RELEASE_ID="$(curl "https://api.github.com/repos/$REPO/releases?access_token=$GH_TOKEN" | jq -r '.[] | select(.tag_name == "v'"$VERSION"'") | .id')"
+
+if [ "$RELEASE_ID" = "" ]; then
+  echo "Error: no release id found" 1>&2
+  exit 1
+fi
 
 echo "Release ID is $RELEASE_ID"
 
-export OUTPUT="launcher"
-
-# should work fine with sonatype releases repo
-./generate-launcher.sh -f -r sonatype:releases
-
-
 # wait for sync to Maven Central
 ATTEMPT=0
-while ! ./generate-launcher.sh -f; do
+while ! $CMD; do
   if [ "$ATTEMPT" -ge 25 ]; then
     echo "Not synced to Maven Central after $ATTEMPT minutes, exiting"
     exit 1
@@ -31,7 +45,14 @@ done
 echo "Uploading launcher"
 
 curl \
-  --data-binary @launcher \
+  --data-binary "@$OUTPUT" \
   -H "Content-Type: application/zip" \
-  "https://uploads.github.com/repos/almond-sh/almond/releases/$RELEASE_ID/assets?name=almond&access_token=$GH_TOKEN"
+  "https://uploads.github.com/repos/$REPO/releases/$RELEASE_ID/assets?name=$NAME&access_token=$GH_TOKEN"
+
+echo "Uploading bat file"
+
+curl \
+  --data-binary "@$OUTPUT.bat" \
+  -H "Content-Type: text/plain" \
+  "https://uploads.github.com/repos/$REPO/releases/$RELEASE_ID/assets?name=$NAME.bat&access_token=$GH_TOKEN"
 
