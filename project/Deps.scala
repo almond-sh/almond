a457c1488f3370cbc5b912cbd26c49ef1dca9628
Scala steward <43047562+scala-steward@users.noreply.github.com>
2019-02-15 12:10:57 +0100

    Update metabrowse-server to 0.2.2 (#316)
    * Update metabrowse-server to 0.2.2
    
    * Remove now unnecessary exclusion

--- project/Deps.scala
+++ project/Deps.scala
@@ -18,7 +18,7 @@ object Deps {
   def fs2 = "co.fs2" %% "fs2-core" % "0.10.7"
   def jeromq = "org.zeromq" % "jeromq" % "0.4.3"
   def jvmRepr = "com.github.jupyter" % "jvm-repr" % "0.4.0"
-  def metabrowseServer = ("org.scalameta" %% "metabrowse-server" % "0.2.1").exclude("org.slf4j", "slf4j-simple")
+  def metabrowseServer = "org.scalameta" %% "metabrowse-server" % "0.2.2"
   def scalaReflect = setting("org.scala-lang" % "scala-reflect" % scalaVersion.value)
   def scalaRx = "com.lihaoyi" %% "scalarx" % "0.4.0"
   def scalatags = "com.lihaoyi" %% "scalatags" % "0.6.7"
