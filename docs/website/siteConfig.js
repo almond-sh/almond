9b162e2aefb6542cb5970701234dc8c065835231
Alexandre Archambault <alexandre.archambault@gmail.com>
2019-02-15 15:08:06 +0100

    Disable blog
    Until there are actual blog posts

--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -16,7 +16,6 @@ const siteConfig = {
 
   headerLinks: [
     {doc: 'intro', label: 'Docs'},
-    {blog: true, label: 'Blog'},
     {href: 'https://github.com/almond-sh/almond', label: 'GitHub'},
   ],
 
