548dc10feedd422ced651b8e1a0d7b3865d343da
Shadaj Laddad <shadaj@users.noreply.github.com>
2019-03-08 00:23:33 -0800

    Handle connection key being empty to support Google Colaboratory (#329)

--- modules/shared/channels/src/test/scala/almond/channels/zeromq/ZeromqSocketTests.scala
+++ modules/shared/channels/src/test/scala/almond/channels/zeromq/ZeromqSocketTests.scala
@@ -83,6 +83,69 @@ object ZeromqSocketTests extends TestSuite {
       t.unsafeRunSync()
     }
 
+    'simpleWithNoKey - {
+
+      val repEc = ExecutionContext.fromExecutorService(
+        Executors.newSingleThreadExecutor()
+      )
+      val reqEc = ExecutionContext.fromExecutorService(
+        Executors.newSingleThreadExecutor()
+      )
+      val port = ConnectionParameters.randomPort()
+
+      val key = Secret("") // having no key disables signature checking
+
+      val logCtx = LoggerContext.nop
+
+      val rep = ZeromqSocket(
+        repEc,
+        ZMQ.REP,
+        bind = true,
+        s"tcp://localhost:$port",
+        None,
+        None,
+        ctx,
+        key,
+        "hmac-sha256",
+        logCtx
+      )
+
+      val req = ZeromqSocket(
+        reqEc,
+        ZMQ.REQ,
+        bind = false,
+        s"tcp://localhost:$port",
+        None,
+        None,
+        ctx,
+        key,
+        "hmac-sha256",
+        logCtx
+      )
+
+      val msg = Message(
+        Nil,
+        "header",
+        "parent_header",
+        "metadata",
+        "content"
+      )
+
+      val t =
+        for {
+          _ <- rep.open
+          _ <- req.open
+          _ <- req.send(msg)
+          readOpt <- rep.read
+          _ = assert(readOpt.contains(msg))
+          // FIXME Closing should be enforced via bracketing
+          _ <- req.close
+          _ <- rep.close
+        } yield ()
+
+      t.unsafeRunSync()
+    }
+
   }
 
 }
