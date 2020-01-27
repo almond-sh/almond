544a53f6e31c3c371ee254e230cd7ce1095acf4d
Alexandre Archambault <alexandre.archambault@gmail.com>
2020-01-27 11:31:43 +0100

    Switch to jsoniter-scala 2.1.6

--- project/Deps.scala
+++ project/Deps.scala
@@ -8,7 +8,7 @@ object Deps {
   object Versions {
     def ammonite = "1.7.4"
     def caseApp = "2.0.0-M10"
-    def jsoniterScala = "2.0.1"
+    def jsoniterScala = "2.1.6"
   }
 
   def ammoniteRepl = setting(("com.lihaoyi" % "ammonite-repl" % Versions.ammonite).cross(CrossVersion.full))
