9d3929a426ed1862128c3ab32206a54ce9b00c35
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-02-10 00:36:40 +0100

    Update coursier to 2.0.0-RC6-3 (#501)

--- project/Deps.scala
+++ project/Deps.scala
@@ -16,7 +16,7 @@ object Deps {
   def ammoniteSpark = "sh.almond" %% "ammonite-spark" % "0.9.0"
   def caseAppAnnotations = "com.github.alexarchambault" %% "case-app-annotations" % Versions.caseApp
   def caseApp = "com.github.alexarchambault" %% "case-app" % Versions.caseApp
-  def coursier = "io.get-coursier" %% "coursier" % "2.0.0-RC6-2"
+  def coursier = "io.get-coursier" %% "coursier" % "2.0.0-RC6-3"
   def coursierApi = "io.get-coursier" % "interface" % "0.0.18"
   def directories = "io.github.soc" % "directories" % "11"
   def fs2 = "co.fs2" %% "fs2-core" % "2.2.2"
