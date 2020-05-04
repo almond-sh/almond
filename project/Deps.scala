6164fcfa73353cbfab8ab790cdfd4b6db648647f
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-05-04 12:22:02 +0200

    Update scalatags to 0.9.1 (#555)

--- project/Deps.scala
+++ project/Deps.scala
@@ -28,7 +28,7 @@ object Deps {
   def metabrowseServer = "org.scalameta" %% "metabrowse-server" % "0.2.3"
   def scalaReflect = setting("org.scala-lang" % "scala-reflect" % scalaVersion.value)
   def scalaRx = "com.lihaoyi" %% "scalarx" % "0.4.2"
-  def scalatags = "com.lihaoyi" %% "scalatags" % "0.9.0"
+  def scalatags = "com.lihaoyi" %% "scalatags" % "0.9.1"
   def slf4jNop = "org.slf4j" % "slf4j-nop" % "1.8.0-beta4"
 
   def sparkSql = "org.apache.spark" %% "spark-sql" % "2.4.0"
