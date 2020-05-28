f7bf27fbcee8ec3d1564354e3123a2c31682ca7a
Scala Steward <me@scala-steward.org>
2020-05-28 22:07:17 +0200

    Update scalarx to 0.4.3

--- project/Deps.scala
+++ project/Deps.scala
@@ -27,7 +27,7 @@ object Deps {
   def jvmRepr = "com.github.jupyter" % "jvm-repr" % "0.4.0"
   def metabrowseServer = "org.scalameta" %% "metabrowse-server" % "0.2.3"
   def scalaReflect = setting("org.scala-lang" % "scala-reflect" % scalaVersion.value)
-  def scalaRx = "com.lihaoyi" %% "scalarx" % "0.4.2"
+  def scalaRx = "com.lihaoyi" %% "scalarx" % "0.4.3"
   def scalatags = "com.lihaoyi" %% "scalatags" % "0.9.1"
   def slf4jNop = "org.slf4j" % "slf4j-nop" % "1.8.0-beta4"
 
