359483f28fed3cc5632136ebee3aad17bd89fc67
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-06-17 13:56:06 +0200

    Update jsoniter-scala-core, ... to 2.2.6 (#584)

--- project/Deps.scala
+++ project/Deps.scala
@@ -8,7 +8,7 @@ object Deps {
   object Versions {
     def ammonite = "2.0.4"
     def caseApp = "2.0.0"
-    def jsoniterScala = "2.2.5"
+    def jsoniterScala = "2.2.6"
   }
 
   def ammoniteRepl = setting(("com.lihaoyi" % "ammonite-repl" % Versions.ammonite).cross(CrossVersion.full))
