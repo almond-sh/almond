4722981ef17e715c7438c961e7751e0c34cb29cf
Scala Steward <me@scala-steward.org>
2020-07-20 14:04:27 +0200

    Update ammonite-repl, ammonite-repl-api to 2.1.4-13-fb16e4e

--- project/Deps.scala
+++ project/Deps.scala
@@ -6,7 +6,7 @@ import sbt.Keys.scalaVersion
 object Deps {
 
   object Versions {
-    def ammonite = "2.1.4-11-307f3d8"
+    def ammonite = "2.1.4-13-fb16e4e"
     def caseApp = "2.0.3"
     def jsoniterScala = "2.5.0"
   }
