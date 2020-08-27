48ea0aecf5c1764b227a5bc67725953b4450e2a4
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-08-27 11:45:16 +0200

    Update interface to 0.0.25 (#640)

--- project/Deps.scala
+++ project/Deps.scala
@@ -17,7 +17,7 @@ object Deps {
   def caseAppAnnotations = "com.github.alexarchambault" %% "case-app-annotations" % Versions.caseApp
   def caseApp = "com.github.alexarchambault" %% "case-app" % Versions.caseApp
   def coursier = "io.get-coursier" %% "coursier" % "2.0.0-RC6-24"
-  def coursierApi = "io.get-coursier" % "interface" % "0.0.24"
+  def coursierApi = "io.get-coursier" % "interface" % "0.0.25"
   def directories = "io.github.soc" % "directories" % "12"
   def fs2 = "co.fs2" %% "fs2-core" % "2.4.4"
   def jansi = "org.fusesource.jansi" % "jansi" % "1.18"
