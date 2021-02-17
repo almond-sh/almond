7612fb14836f35b0fe59c6f321d2fc1b9594504a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-02-17 20:11:18 +0100

    Update jsoniter-scala-core, ... to 2.6.4 (#738)

--- project/Deps.scala
+++ project/Deps.scala
@@ -8,7 +8,7 @@ object Deps {
   object Versions {
     def ammonite = "2.3.8-4-88785969"
     def caseApp = "2.0.4"
-    def jsoniterScala = "2.6.2"
+    def jsoniterScala = "2.6.4"
     def scalafmt = "2.7.5"
   }
 
