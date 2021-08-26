3e15cdef3f65aa663567a0bfa796ca02909ca95b
Alexandre Archambault <alexandre.archambault@gmail.com>
2021-08-26 23:05:30 +0200

    Build a Scala 3 docker image

--- scripts/update-docker-images.sh
+++ scripts/update-docker-images.sh
@@ -5,8 +5,7 @@ set -eu
 
 SCALA212_VERSION="$(./mill scala212)"
 SCALA213_VERSION="$(./mill scala213)"
-
-ALMOND_VERSION="$(git describe --tags --abbrev=0 --match 'v*' | sed 's/^v//')"
+SCALA3_VERSION="$(./mill scala3)"
 
 DOCKER_REPO=almondsh/almond
 
@@ -18,22 +17,29 @@ if [[ ${TAG} != v* ]]; then
   echo "Not on a git tag, creating snapshot image"
   ALMOND_VERSION="$(./mill show 'scala.scala-kernel['"$SCALA213_VERSION"'].publishVersion' | jq -r .)"
   IMAGE_NAME=${DOCKER_REPO}:snapshot
+  ./mill '__['"$SCALA3_VERSION"'].publishLocal'
   ./mill '__['"$SCALA213_VERSION"'].publishLocal'
   ./mill '__['"$SCALA212_VERSION"'].publishLocal'
   cp -r $HOME/.ivy2/local/ ivy-local/
   docker build --build-arg ALMOND_VERSION=${ALMOND_VERSION} --build-arg=LOCAL_IVY=yes \
-    --build-arg SCALA_VERSIONS="$SCALA213_VERSION $SCALA212_VERSION" -t ${IMAGE_NAME} .
+    --build-arg SCALA_VERSIONS="$SCALA3_VERSION $SCALA213_VERSION $SCALA212_VERSION" \
+    -t ${IMAGE_NAME} .
   docker push ${IMAGE_NAME}
 else
+  ALMOND_VERSION="$(git describe --tags --abbrev=0 --match 'v*' | sed 's/^v//')"
   echo "Creating release images for almond ${ALMOND_VERSION}"
   IMAGE_NAME=${DOCKER_REPO}:${ALMOND_VERSION}
   docker build --build-arg ALMOND_VERSION=${ALMOND_VERSION} \
-    --build-arg SCALA_VERSIONS="$SCALA213_VERSION $SCALA212_VERSION" -t ${IMAGE_NAME} .
+    --build-arg SCALA_VERSIONS="$SCALA3_VERSION $SCALA213_VERSION $SCALA212_VERSION" \
+    -t ${IMAGE_NAME} .
+  docker build --build-arg ALMOND_VERSION=${ALMOND_VERSION} \
+    --build-arg SCALA_VERSIONS="$SCALA3_VERSION" -t ${IMAGE_NAME}-scala-${SCALA3_VERSION} .
   docker build --build-arg ALMOND_VERSION=${ALMOND_VERSION} \
     --build-arg SCALA_VERSIONS="$SCALA213_VERSION" -t ${IMAGE_NAME}-scala-${SCALA213_VERSION} .
   docker build --build-arg ALMOND_VERSION=${ALMOND_VERSION} \
     --build-arg SCALA_VERSIONS="$SCALA212_VERSION" -t ${IMAGE_NAME}-scala-${SCALA212_VERSION} .
 
+  docker push ${IMAGE_NAME}-scala-${SCALA3_VERSION}
   docker push ${IMAGE_NAME}-scala-${SCALA213_VERSION}
   docker push ${IMAGE_NAME}-scala-${SCALA212_VERSION}
   docker push ${IMAGE_NAME}
