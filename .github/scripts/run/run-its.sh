0fc846572169384e1621790342026dfc24a44040
Alex Archambault <alexandre.archambault@gmail.com>
2023-06-08 18:44:32 +0200

    Run all ITs without Mill process being around

---
+++ .github/scripts/run/run-its.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -eu
+
+SCALA_CLI="scala-cli"
+RUN_APP="./run"
+
+if [ "$(expr substr $(uname -s) 1 5 2>/dev/null)" == "MINGW" ]; then
+  SCALA_CLI="$SCALA_CLI.bat"
+  RUN_APP="$RUN_APP.exe"
+fi
+
+checkResults() {
+  RESULTS="$(jq -r '.[1] | map(.status) | join("\n")' < test-output.json | sort -u)"
+  if [ "$RESULTS" != "Success" ]; then
+    exit 1
+  fi
+}
+
+"$SCALA_CLI" --power package .github/scripts/run --native-image -o "$RUN_APP" -- --no-fallback
+
+trap "jps -mlv" EXIT
+
+if [ "$(expr substr $(uname -s) 1 5 2>/dev/null)" == "MINGW" ]; then
+  ./mill -i show "scala.integration.test.testCommand" "almond.integration.KernelTestsTwoStepStartup212.*" > test-args-212.json
+  cat test-args-212.json
+  "$RUN_APP" test-args-212.json
+  checkResults
+
+  ./mill -i show "scala.integration.test.testCommand" "almond.integration.KernelTestsTwoStepStartup213.*" > test-args-213.json
+  cat test-args-213.json
+  "$RUN_APP" test-args-213.json
+  checkResults
+
+  ./mill -i show "scala.integration.test.testCommand" "almond.integration.KernelTestsTwoStepStartup3.*" > test-args-3.json
+  cat test-args-3.json
+  "$RUN_APP" test-args-3.json
+  checkResults
+else
+  ./mill -i show "scala.integration.test.testCommand" > test-args.json
+  cat test-args.json
+  "$RUN_APP" test-args.json
+  checkResults
+fi
