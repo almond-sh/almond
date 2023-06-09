3d1a1c110bdaa6691fd9a38bf359302fabbb59d9
Alex Archambault <alexandre.archambault@gmail.com>
2023-06-09 11:20:24 +0200

    Add logging / checks when spawning kernel in ITs

--- modules/scala/integration/src/main/scala/almond/integration/KernelLauncher.scala
+++ modules/scala/integration/src/main/scala/almond/integration/KernelLauncher.scala
@@ -485,6 +485,7 @@ class KernelLauncher(
           else
             Map.empty[String, String]
 
+        assert(proc == null)
         proc = os.proc(command).spawn(
           cwd = dir,
           env = extraEnv ++ specExtraEnv,
