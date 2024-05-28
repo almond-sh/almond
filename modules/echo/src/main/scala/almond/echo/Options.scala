c39fa0ebde9e2882fb9f5c5462fe9be8e566a3bb
Alexandre Archambault <alexandre.archambault@gmail.com>
2024-05-28 17:20:50 +0200

    NIT Formatting

--- modules/echo/src/main/scala/almond/echo/Options.scala
+++ modules/echo/src/main/scala/almond/echo/Options.scala
@@ -7,19 +7,22 @@ import caseapp.core.parser.Parser
 import caseapp.Hidden
 import scala.concurrent.duration.{Duration, DurationInt}
 
+// format: off
 final case class Options(
   connectionFile: Option[String] = None,
   @HelpMessage("Log level (one of none, error, warn, info, or debug)")
-  log: String = "warn",
+    log: String = "warn",
   install: Boolean = false,
   @Recurse
-  installOptions: InstallOptions = InstallOptions(),
+    installOptions: InstallOptions = InstallOptions(),
+
   @HelpMessage(
     """Time given to the client to accept ZeroMQ messages before exiting. Parsed with scala.concurrent.duration.Duration, this accepts things like "Inf" or "5 seconds""""
   )
   @Hidden
-  linger: Option[String] = None
+    linger: Option[String] = None
 ) {
+  // format: on
 
   lazy val lingerDuration = linger
     .map(_.trim)
