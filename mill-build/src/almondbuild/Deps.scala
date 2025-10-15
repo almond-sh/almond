bb9780ccdd3647ec2966115b2c4ab7b7d13d7597
Scala Steward <scala_steward@virtuslab.com>
2025-10-15 13:29:49 +0000

    Update munit to 1.2.1

--- mill-build/src/almondbuild/Deps.scala
+++ mill-build/src/almondbuild/Deps.scala
@@ -57,7 +57,7 @@ object Deps {
   def jvmRepr                  = ivy"com.github.jupyter:jvm-repr:0.4.0"
   def mdoc                     = ivy"org.scalameta::mdoc:2.6.5"
   def mtags                    = ivy"org.scalameta:::mtags:1.5.1"
-  def munit                    = ivy"org.scalameta::munit:1.1.1"
+  def munit                    = ivy"org.scalameta::munit:1.2.1"
   def osLib                    = ivy"com.lihaoyi::os-lib:0.11.5"
   def pprint                   = ivy"com.lihaoyi::pprint:0.9.0"
   def scalafmtDynamic          = ivy"org.scalameta::scalafmt-dynamic:${Versions.scalafmt}"
