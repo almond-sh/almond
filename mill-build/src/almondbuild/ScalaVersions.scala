1852b5abddc6909899b84ab6230c3081a2a4aec5
Scala Steward <scala_steward@virtuslab.com>
2025-10-15 13:29:18 +0000

    Update scala3-library_3 to 3.3.7

--- mill-build/src/almondbuild/ScalaVersions.scala
+++ mill-build/src/almondbuild/ScalaVersions.scala
@@ -2,7 +2,7 @@ package almondbuild
 
 object ScalaVersions {
   def scala3Latest   = "3.6.3"
-  def scala3Compat   = "3.3.4"
+  def scala3Compat   = "3.3.7"
   def scala213       = "2.13.16"
   def scala212       = "2.12.20"
   val binaries       = Seq(scala3Compat, scala213, scala212)
