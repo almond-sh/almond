b5bbf0424d2f02dfe7ac0d4d7f7a0e1c97d25273
Scala Steward <scala_steward@virtuslab.com>
2025-10-15 13:28:16 +0000

    Update utest to 0.9.1

--- mill-build/src/almondbuild/Deps.scala
+++ mill-build/src/almondbuild/Deps.scala
@@ -71,5 +71,5 @@ object Deps {
   def testUtil                 = ivy"io.github.alexarchambault::test-util:0.1.7"
   def upickle =
     ivy"com.lihaoyi::upickle:3.1.4" // trying to use the same version as Ammonite, to avoid bin compat issues
-  def utest = ivy"com.lihaoyi::utest:0.9.0"
+  def utest = ivy"com.lihaoyi::utest:0.9.1"
 }
