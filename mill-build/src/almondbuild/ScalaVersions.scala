a3e3ad79c677c159184b2a2ba8f6d6acaf7636b5
Scala Steward <scala_steward@virtuslab.com>
2025-10-15 13:29:04 +0000

    Update scala-library, scala-reflect to 2.13.17

--- mill-build/src/almondbuild/ScalaVersions.scala
+++ mill-build/src/almondbuild/ScalaVersions.scala
@@ -3,7 +3,7 @@ package almondbuild
 object ScalaVersions {
   def scala3Latest   = "3.6.3"
   def scala3Compat   = "3.3.4"
-  def scala213       = "2.13.16"
+  def scala213       = "2.13.17"
   def scala212       = "2.12.20"
   val binaries       = Seq(scala3Compat, scala213, scala212)
   val scala2Binaries = Seq(scala213, scala212)
