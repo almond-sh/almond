f07e9b159d8c46f978ada4c5379f18bb91d807c1
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-10-23 20:22:06 +0200

    Update coursier, coursier-launcher_2.13 to 2.1.25-M19 (#1592)
    * Update coursier, coursier-launcher_2.13 to 2.1.25-M19
    
    * Fix compilation
    
    ---------
    
    Co-authored-by: Alex Archambault <alexandre.archambault@gmail.com>

--- mill-build/src/almondbuild/Versions.scala
+++ mill-build/src/almondbuild/Versions.scala
@@ -3,7 +3,7 @@ package almondbuild
 object Versions {
   def ammonite      = "3.0.4"
   def caseApp       = "2.1.0"
-  def coursier      = "2.1.25-M2"
+  def coursier      = "2.1.25-M19"
   def jsoniterScala = "2.13.5"
   def scalafmt      = "3.9.6"
   def scalafmtJava8 = "3.7.15"
