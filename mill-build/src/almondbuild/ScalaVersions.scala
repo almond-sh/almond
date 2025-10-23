cb319a6b82f97f59b1f2b76539de1dfefa046c93
Alexandre Archambault <alexarchambault@users.noreply.github.com>
2025-10-23 22:02:10 +0200

    Update Scala versions (#1603)
    * Update mtags to 1.6.3
    
    * Update Scala versions

--- mill-build/src/almondbuild/ScalaVersions.scala
+++ mill-build/src/almondbuild/ScalaVersions.scala
@@ -1,22 +1,30 @@
 package almondbuild
 
 object ScalaVersions {
-  def scala3Latest   = "3.6.3"
+  def scala3Latest   = "3.7.3"
   def scala3Compat   = "3.3.4"
-  def scala213       = "2.13.16"
+  def scala213       = "2.13.17"
   def scala212       = "2.12.20"
   val binaries       = Seq(scala3Compat, scala213, scala212)
   val scala2Binaries = Seq(scala213, scala212)
   val all = Seq(
     scala3Latest,
+    "3.6.4",
+    "3.6.3",
+    "3.6.2",
+    "3.5.2",
+    "3.5.1",
+    "3.5.0",
     "3.4.3",
+    "3.4.2",
+    "3.3.7",
+    "3.3.6",
     "3.3.5",
     scala3Compat,
     scala213,
+    "2.13.16",
     "2.13.15",
     "2.13.14",
-    "2.13.13",
-    "2.13.12",
     scala212,
     "2.12.19",
     "2.12.18"
