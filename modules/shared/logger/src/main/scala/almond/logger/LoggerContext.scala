4496275750f8a2cf0f491c31aa82ee4cf183cc1d
Alex Archambault <alexandre.archambault@gmail.com>
2025-11-27 11:30:55 +0100

    Don't log things in bold if log colors are disabled

--- modules/shared/logger/src/main/scala/almond/logger/LoggerContext.scala
+++ modules/shared/logger/src/main/scala/almond/logger/LoggerContext.scala
@@ -7,14 +7,21 @@ import almond.logger.internal.LoggerContextImpl
 trait LoggerContext {
   def apply(prefix: String): Logger
 
+  def colored: Boolean
+
   final def apply(clazz: Class[_]): Logger =
-    apply(Console.BOLD + clazz.getSimpleName.stripSuffix("$") + Console.RESET + " ")
+    apply(
+      if (colored)
+        Console.BOLD + clazz.getSimpleName.stripSuffix("$") + Console.RESET + " "
+      else
+        clazz.getSimpleName.stripSuffix("$") + " "
+    )
 }
 
 object LoggerContext {
 
   def nop: LoggerContext =
-    LoggerContextImpl(Logger.nop)
+    LoggerContextImpl(Logger.nop, colored = false)
 
   def printStream(
     level: Level,
@@ -22,16 +29,28 @@ object LoggerContext {
     colored: Boolean,
     addPid: Boolean
   ): LoggerContext =
-    LoggerContextImpl(Logger.printStream(level, out, colored = colored, addPid = addPid))
+    LoggerContextImpl(
+      Logger.printStream(level, out, colored = colored, addPid = addPid),
+      colored = colored
+    )
 
   def printStream(level: Level, out: PrintStream, colored: Boolean): LoggerContext =
-    LoggerContextImpl(Logger.printStream(level, out, colored = colored))
+    LoggerContextImpl(
+      Logger.printStream(level, out, colored = colored),
+      colored = colored
+    )
 
   def printStream(level: Level, out: PrintStream): LoggerContext =
-    LoggerContextImpl(Logger.printStream(level, out))
+    LoggerContextImpl(
+      Logger.printStream(level, out),
+      colored = true
+    )
 
   def stderr(level: Level, colored: Boolean, addPid: Boolean): LoggerContext =
-    LoggerContextImpl(Logger.stderr(level, colored, addPid))
+    LoggerContextImpl(
+      Logger.stderr(level, colored, addPid),
+      colored = colored
+    )
 
   def stderr(level: Level, colored: Boolean): LoggerContext =
     stderr(level, colored, addPid = false)
