77c60ab3baaf3a1df8450292b0e51c0137550ac4
Alexandre Archambault <alexarchambault@users.noreply.github.com>
2025-11-28 11:12:27 +0100

    Don't log things in bold if log colors are disabled (#1615)

--- modules/shared/logger/src/main/scala/almond/logger/internal/LoggerContextImpl.scala
+++ modules/shared/logger/src/main/scala/almond/logger/internal/LoggerContextImpl.scala
@@ -2,7 +2,7 @@ package almond.logger.internal
 
 import almond.logger.{Logger, LoggerContext}
 
-final case class LoggerContextImpl(baseLogger: Logger) extends LoggerContext {
+final case class LoggerContextImpl(baseLogger: Logger, colored: Boolean) extends LoggerContext {
   def apply(prefix: String): Logger =
     baseLogger.prefix(prefix)
 }
