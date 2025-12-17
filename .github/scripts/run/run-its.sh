2bef06a9da60652e55ea83e0d942b4c02e5f5118
Alexandre Archambault <alexarchambault@users.noreply.github.com>
2025-12-17 13:24:50 +0100

    Drop Scala 3 support in 0.14.1 branch (#1622)

--- .github/scripts/run/run-its.sh
+++ .github/scripts/run/run-its.sh
@@ -20,25 +20,7 @@ checkResults() {
 
 trap "jps -mlv" EXIT
 
-if [ "$(expr substr $(uname -s) 1 5 2>/dev/null)" == "MINGW" ]; then
-  ./mill -i show "scala.integration.test.testCommand" "almond.integration.KernelTestsTwoStepStartup212.*" > test-args-212.json
-  ./mill -i show "scala.integration.test.testCommand" "almond.integration.KernelTestsTwoStepStartup213.*" > test-args-213.json
-  ./mill -i show "scala.integration.test.testCommand" "almond.integration.KernelTestsTwoStepStartup3.*" > test-args-3.json
-
-  cat test-args-212.json
-  "$RUN_APP" test-args-212.json
-  checkResults
-
-  cat test-args-213.json
-  "$RUN_APP" test-args-213.json
-  checkResults
-
-  cat test-args-3.json
-  "$RUN_APP" test-args-3.json
-  checkResults
-else
-  ./mill -i show "scala.integration.test.testCommand" > test-args.json
-  cat test-args.json
-  "$RUN_APP" test-args.json
-  checkResults
-fi
+./mill -i show "scala.integration.test.testCommand" > test-args.json
+cat test-args.json
+"$RUN_APP" test-args.json
+checkResults
--- modules/scala/integration/src/test/scala/almond/integration/KernelTestsTwoStepStartup212.scala
+++
@@ -1,8 +0,0 @@
-package almond.integration
-
-class KernelTestsTwoStepStartup212 extends KernelTestsDefinitions {
-
-  lazy val kernelLauncher =
-    new KernelLauncher(KernelLauncher.LauncherType.Jvm, KernelLauncher.testScala212Version)
-
-}
