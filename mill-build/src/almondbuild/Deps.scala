b334880bacb653ece1cb628553c9a4a01efb2996
Scala Steward <scala_steward@virtuslab.com>
2026-06-10 13:32:35 +0000

    Update versions to 0.5.3

--- mill-build/src/almondbuild/Deps.scala
+++ mill-build/src/almondbuild/Deps.scala
@@ -43,7 +43,7 @@ object Deps {
     mvn"io.get-coursier::coursier:${Versions.coursier}".exclude(("org.slf4j", "slf4j-api"))
   def coursierApi         = mvn"io.get-coursier:interface:1.0.29-M2"
   def coursierLauncher    = mvn"io.get-coursier:coursier-launcher_2.13:${Versions.coursier}"
-  def coursierVersions    = mvn"io.get-coursier::versions:0.5.1"
+  def coursierVersions    = mvn"io.get-coursier::versions:0.5.3"
   def dependencyInterface = mvn"io.get-coursier::dependency-interface:0.3.2"
   def directiveHandler    = mvn"io.github.alexarchambault.scala-cli::directive-handler:0.1.4"
   def expecty             = mvn"com.eed3si9n.expecty::expecty:0.17.0"
