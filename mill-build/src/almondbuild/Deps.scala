cb6ece321b03c7d0321465788135b2f8bc5c7352
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-06-17 14:39:55 +0200

    Update mdoc to 2.8.2 (#1686)

--- mill-build/src/almondbuild/Deps.scala
+++ mill-build/src/almondbuild/Deps.scala
@@ -56,7 +56,7 @@ object Deps {
   def jsoniterScalaMacros =
     mvn"com.github.plokhotnyuk.jsoniter-scala::jsoniter-scala-macros:${Versions.jsoniterScala}"
   def jvmRepr                  = mvn"com.github.jupyter:jvm-repr:0.4.0"
-  def mdoc                     = mvn"org.scalameta::mdoc:2.8.0"
+  def mdoc                     = mvn"org.scalameta::mdoc:2.8.2"
   def mtags                    = mvn"org.scalameta:::mtags:1.6.3"
   def munit                    = mvn"org.scalameta::munit:1.2.4"
   def osLib                    = mvn"com.lihaoyi::os-lib:0.11.5"
