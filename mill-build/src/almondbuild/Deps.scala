9c353268f98300af3bcc20986cfec11513c1dd4e
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-09-17 19:38:47 +0200

    Merge pull request #1736 from scala-steward/update/versions-0.6.0
    Update versions to 0.6.0

--- mill-build/src/almondbuild/Deps.scala
+++ mill-build/src/almondbuild/Deps.scala
@@ -46,7 +46,7 @@ object Deps {
   def coursierApi         = mvn"io.get-coursier:interface:1.0.29-M4"
   def coursierLauncher    = mvn"io.get-coursier:coursier-launcher_2.13:${Versions.coursier}"
   def coursierPaths       = mvn"io.get-coursier:coursier-paths:${Versions.coursier}"
-  def coursierVersions    = mvn"io.get-coursier::versions:0.5.3"
+  def coursierVersions    = mvn"io.get-coursier::versions:0.6.0"
   def dependency          = mvn"io.get-coursier::dependency:0.3.2"
   def dependencyInterface = mvn"io.get-coursier::dependency-interface:0.3.2"
   def directiveHandler    = mvn"io.github.alexarchambault.scala-cli::directive-handler:0.1.4"
