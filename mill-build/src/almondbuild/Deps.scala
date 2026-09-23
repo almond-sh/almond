dfdda7432fec78bd4e808d9fbd85e9f19b87c4d1
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-09-23 16:15:36 +0200

    Update versions to 0.6.1 (#1744)

--- mill-build/src/almondbuild/Deps.scala
+++ mill-build/src/almondbuild/Deps.scala
@@ -44,7 +44,7 @@ object Deps {
   def coursierApi         = mvn"io.get-coursier:interface:1.0.29"
   def coursierLauncher    = mvn"io.get-coursier:coursier-launcher_2.13:${Versions.coursier}"
   def coursierPaths       = mvn"io.get-coursier:coursier-paths:${Versions.coursier}"
-  def coursierVersions    = mvn"io.get-coursier::versions:0.6.0"
+  def coursierVersions    = mvn"io.get-coursier::versions:0.6.1"
   def dependency          = mvn"io.get-coursier::dependency:0.3.3"
   def dependencyInterface = mvn"io.get-coursier::dependency-interface:0.3.2"
   def directiveHandler    = mvn"io.github.alexarchambault.scala-cli::directive-handler:0.1.4"
